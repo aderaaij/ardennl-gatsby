6d47f06248d60e9e8c25eb2c8f5d1228d6fc1235
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-01 22:15:32 +0000

    [remove] bloglist from home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import get from 'lodash/get';
 
 class Home extends React.Component {
     render() {
         const { edges } = this.props.data.allMarkdownRemark;
         return (
             <div>
-                {edges.map(({ node }) => (
-                    <div key={node.id}>
-                        <Link
-                            to={node.fields.slug}
-                        >
-                            <img src=
-                                { node.frontmatter.cover ? node.frontmatter.cover.childImageSharp.resolutions.src : '' }/>
-                            <h3>
-                                {node.frontmatter.title}{' '}
-                                <span>— {node.frontmatter.date}</span>
-                            </h3>
-                            <p>{node.excerpt}</p>
-                        </Link>
-                    </div>
-                ))}
+                Hello
             </div>
         );
     }
