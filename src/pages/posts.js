006cc8dbd1aa4241ff2ac251b84b94a4994f27ba
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-03 16:00:41 +0000

    [add] resize graphql info on postpage

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -5,6 +5,7 @@ import PostsList from '../components/PostsList/PostsList';
 
 const BlogList = (props) => {
     const { edges } = props.data.allMarkdownRemark;
+    console.log(edges);
     return (
         <PostsList edges={edges} />
     );
@@ -25,11 +26,23 @@ export const query = graphql`
                     frontmatter {
                         title
                         tags
+                        category
                         date(formatString: "DD MMMM, YYYY")
                         cover {
                             id
                             relativePath
                             childImageSharp {
+                                resize(
+                                    width: 400, 
+                                    height: 300
+                                ) {
+                                    src
+                                    tracedSVG
+                                    width
+                                    height
+                                    aspectRatio
+                                    originalName
+                                }
                                 sizes(
                                     maxWidth: 400,
                                     traceSVG: {
