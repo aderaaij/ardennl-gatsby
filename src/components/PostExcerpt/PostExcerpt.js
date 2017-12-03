6bb72aeee2d506f095866e416fe5cce612f25d4b
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-03 16:01:19 +0000

    [switch] styling of categorie / data

--- src/components/PostExcerpt/PostExcerpt.js
+++ src/components/PostExcerpt/PostExcerpt.js
@@ -25,60 +25,120 @@ const BlogArticle = styled.article`
 `;
 
 const BlogContent = styled.div`
-padding: 1em 0;
-
-@media (min-width: 768px) {
-    padding: 2em;
-}
-
-& span {
-    display: block;
-    margin: 0 0 1em;
-    color: ${mc.blueGrey[300]}
-    text-transform: lowercase;
-    font-style: italic;
+    padding: 1em 0;
 
     @media (min-width: 768px) {
-        font-size: 1.125em;
+        padding: 2em;
     }
-}
 
-h2 {
-    font-size: 1.5em;
-    color: ${mc.cyan[700]};
+    & > span {
+        
+    }
 
-    a {
+    h2 {
+        font-size: 1.5em;
         color: ${mc.cyan[700]};
-        text-decoration: none;
+
+        a {
+            color: ${mc.cyan[700]};
+            text-decoration: none;
+        }
+
+        @media (min-width: 768px) {
+            font-size: 2em;
+        }
     }
 
-    @media (min-width: 768px) {
-        font-size: 2em;
-    }
-}
+    & p {
+        font-size: 1.125em;
+        max-width: 65ch;
+        line-height: 1.5;
+        color: ${mc.blueGrey[800]}
 
-& p {
-    font-size: 1.125em;
-    max-width: 65ch;
-    line-height: 1.5;
-    color: ${mc.blueGrey[800]}
-
-    @media (min-width: 768px) {
-        font-size: 1.375em;
+        @media (min-width: 768px) {
+            font-size: 1.25em;
+        }
     }
-}
 `;
 
-const TagList = styled.ul`
+const ExcerptMeta = styled.ul`
+    color: ${mc.blueGrey[300]};
+    font-style: italic;
+    display: flex;  
+    font-size: 1rem;
     list-style: none;
     margin: 0;
     padding: 0;
+    text-transform: lowercase;
+
+    & ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+    }    
+
+    & > li {
+        font-size: 1rem; 
+
+        &::after {
+            content: '|';
+            margin: 0 0.5em;
+        }
+
+        &:last-child {
+            display: flex;
+
+            &::after {
+                content: '';
+                margin: 0;
+            }
+        }
+
+        &:nth-child(2) {
+            
+        }
+    }
+
+    & a {
+        text-decoration: none;
+        color: ${mc.blueGrey[300]};
+    }
+`;
+
+const TagList = styled.div`
     display: flex;
-    color: ${mc.blueGrey[300]};
-    font-style: italic;
+
+    span {
+        margin-right: 1ch;
+    }
+
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }   
+
+    a {
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
 
     li {
-        margin: 0 0.5em 0 0;
+        &::after {
+            content: ',';
+            margin-right: 1ch;
+        }
+
+        &:last-child {
+            &::after {
+                content: '';
+                margin: 0;
+            }
+        }
     }
 `;
 
@@ -93,6 +153,25 @@ const imgStyle = css`
     }
 `;
 
+const CatLink = styled(Link)`
+    color: ${mc.blueGrey[300]};
+    text-decoration: none;
+
+    display: block;
+    margin: 0 0 1em;
+    color: ${mc.blueGrey[300]}
+    text-transform: lowercase;
+    font-style: italic;
+
+    @media (min-width: 768px) {
+        font-size: 1.125em;
+    }
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
 function goToPage(e, slug) {
     if (e.target.tagName === 'A') {
         e.preventDefault();
@@ -103,24 +182,35 @@ function goToPage(e, slug) {
 
 const PostExcerpt = (props) => {
     const { postInfo } = props;
+    console.log(postInfo);
     return (
         <BlogArticle onClick={e => goToPage(e, postInfo.path)}>
-            <Img className={imgStyle} alt="Picture of X" sizes={postInfo.cover.childImageSharp.sizes} />
+            <Img className={imgStyle} alt="Picture of X" sizes={postInfo.cover.childImageSharp.resize} />
             <BlogContent>
-                <span>{postInfo.date}</span>
+                <CatLink to={`/categories/${postInfo.category}`}>{postInfo.category}</CatLink>
                 <h2>
                     <Link to={postInfo.path}>{postInfo.title}</Link>
                 </h2>
                 <p>{postInfo.excerpt}</p>
-                <TagList>
-                    {postInfo.tags.map(tag => (
-                        <li key={tag}>
-                            <Link to={`/tags/${tag}`}>
-                                {tag}
-                            </Link>
-                        </li>
-                    ))}
-                </TagList>
+                <ExcerptMeta>
+                    <li>
+                        <span>{postInfo.date}</span>
+                    </li>
+                    <li>
+                        <TagList>
+                            <span>Tagged:</span>
+                            <ul>
+                                {postInfo.tags.map(tag => (
+                                    <li key={tag}>
+                                        <Link to={`/tags/${tag}`}>
+                                            {tag}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </TagList>
+                    </li>
+                </ExcerptMeta>
             </BlogContent>
         </BlogArticle>
     );
