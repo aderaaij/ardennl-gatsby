e86ceb37a9bb7e45338d2030c8c2ff292a501a4f
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-03 17:04:51 +0000

    [add] category template

---
+++ src/templates/category.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PostsList from '../components/PostsList/PostsList';
+
+const CategoryTemplate = ({ data }) => {
+    const { edges } = data.allMarkdownRemark;
+    return (
+        <PostsList edges={edges} />
+    );
+};
+
+export default CategoryTemplate;
+
+export const pageQuery = graphql`
+    query CategoryPage {
+        allMarkdownRemark(
+            limit: 1000
+            sort: { fields: [frontmatter___date], order: DESC }
+            
+        ) {
+            totalCount
+            edges {
+                node {
+                    fields {
+                        slug
+                    }
+                    excerpt
+                    timeToRead
+                    frontmatter {
+                        title
+                        tags
+                        category
+                        date(formatString: "DD MMMM, YYYY")
+                        cover {
+                            id
+                            relativePath
+                            childImageSharp {
+                                resize(
+                                    width: 400, 
+                                    height: 300
+                                ) {
+                                    src
+                                    tracedSVG
+                                    width
+                                    height
+                                    aspectRatio
+                                    originalName
+                                }
+                            }
+                        }
+                    }         
+                }
+            }
+        }
+    }
+`;
