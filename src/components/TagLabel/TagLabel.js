db3d4dea6a0343a712c249ab1f09b2984340ad77
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-04 00:04:00 +0000

    [add] a taglabel component which takes in a text and extra styles

---
+++ src/components/TagLabel/TagLabel.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+import styled, { css } from 'react-emotion';
+import Link from 'gatsby-link';
+import mc from 'material-colors';
+
+const Tag = styled.div`
+    display: inline-block;
+    background: ${mc.cyan[700]};
+    border-radius: 4px;
+    line-height: 1;
+    text-transform: uppercase;
+    color: #fff;
+    padding: 0.25em 0.5em;
+    font-family: 'proxima-nova',sans-serif;
+    font-weight: 700;
+`;
+
+const TagLabel = (props) => {
+    console.log(props);
+    return (
+        <Tag className={props.style}>
+            {props.tagText}
+        </Tag>
+    );
+};
+
+export default TagLabel;
