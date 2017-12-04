cf06b08dcc7813280e60a2db77dbc794d7ebfb29
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-04 19:03:38 +0000

    [add] piwik component

---
+++ src/components/Piwik/Piwik.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Piwik = () => (
+    <script type="text/javascript">{`
+        var _paq = _paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+        var u="//analytics.arden.nl/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        })();
+        // End Piwik Code                
+    `}
+    </script>
+);
+
+export default Piwik;
