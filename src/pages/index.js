b0c9e12d33d725cfb3fc978e6b597c11410a7366
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-04 23:00:22 +0000

    [fix] content link (linkedin) and some styling for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,29 +32,44 @@ const HomeContent = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    width: 60vw;
+    width: 100vw;
     height: 100%;
-    display: flex;
     align-items: center;
-    padding: 0 2em;
-    background: rgba(255, 255, 255, 0.9);
+    padding: calc(60px + 1em) 1em 0;
+    background: rgba(255, 255, 255, 1);
+
+    @media (min-width: 1024px) {
+        width: 60vw;
+        display: flex;
+        padding: 0 2em;
+        background: rgba(255, 255, 255, 0.9);
+
+    }
 
     h1, h2 {
         line-height: 1;
     }
 
     & h1 {
-        font-size: 3.5em;
+        font-size: 2.5em;
         color: ${mc.blueGrey[900]};
         margin: 0 0 0.125em;
+        
+        @media (min-width: 1024px) {
+            font-size: 3.5em;
+        }
     }
 
     & h2 {
         margin: 0;
-        font-size: 3em;
+        font-size: 2em;
         font-weight: 900;
         letter-spacing: 0;
         color: ${mc.blueGrey[700]};
+
+        @media (min-width: 1024px) {
+            font-size: 3em;
+        }
     }
 
     p {
@@ -81,8 +96,8 @@ const Home = (props) => {
                     <h1>Arden de Raaij</h1>
                     <h2>Front-end/Web Developer</h2>
                     <div>
-                        <p>I create awesome websites/web-apps which are enjoyable and fun to use. You can find some of my code on <a href="https://github.com/aderaaij/" target="_blank" rel="noopener">Github</a>, see my experiments on <a href="https://codepen.io/aderaaij/" target="_blank" rel="noopener">Codepen</a>, and view my CV/experience on <a href="https&colon;//www.linkedin.com/in/ardenderaaij/" target="_blank" rel="noopener">LinkedIn</a>.</p>
-                        <p>I love photography and traveling, the combination of which you can find on my <a href="http://instagram.com/ardennl" target="_blank" title="Arden de Raaij on Instagram" rel="noopener">Instagram</a>. You can follow me on <a href="http://twitter.com/ardennl" target="_blank" rel="noopener">Twitter</a> for random shower thoughts.</p> <p>Feel free to <a href="mailto:a.de.raaij@gmail.com" target="_blank" title="Send an e-mail to Arden de Raaij" rel="noopener">contact me</a> about all the things web related!</p>
+                        <p>I create awesome websites/web-apps which are enjoyable and fun to use. You can find some of my code on <a href="https://github.com/aderaaij/" target="_blank" rel="noopener noreferrer">Github</a>, see my experiments on <a href="https://codepen.io/aderaaij/" target="_blank" rel="noopener noreferrer">Codepen</a>, and view my CV/experience on <a href="https://www.linkedin.com/in/ardenderaaij/" target="_blank" rel="noopener noreferrer">LinkedIn</a>.</p>
+                        <p>I love photography and traveling, the combination of which you can find on my <a href="http://instagram.com/ardennl" target="_blank noreferrer" title="Arden de Raaij on Instagram" rel="noopener">Instagram</a>. You can follow me on <a href="http://twitter.com/ardennl" target="_blank noreferrer" rel="noopener">Twitter</a> for random shower thoughts.</p> <p>Feel free to <a href="mailto:a.de.raaij@gmail.com" target="_blank noreferrer" title="Send an e-mail to Arden de Raaij" rel="noopener">contact me</a> about all the things web related!</p>
                     </div>
                 </div>
             </HomeContent>
