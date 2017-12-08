753dc71db2f6329806a85e7ffaf2d3728a6541f1
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-08 02:54:51 +0000

    [fix] mobile layout, images

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,21 +4,23 @@ import Helmet from 'react-helmet';
 import Img from 'gatsby-image';
 import styled, { css } from 'react-emotion';
 import Link from 'gatsby-link';
-import mc from 'material-colors';
 import SEO from '../components/SEO/SEO';
 import Tag from '../components/TagLabel/TagLabel';
-
+import { colorScheme } from '../helpers/styleSettings';
+import { GridBase, ContentLimit } from '../helpers/grid';
 import './b16-tomorrow-dark.css';
+
 // import 'intersection-observer';
 
+
 const Article = styled.article`
-    padding-top: 60px;
+    // padding-top: 60px;
 `;
 
 const imgStyle = css`
     width: 100%;
     height: 100%;
-    position: relative;
+    position: absolute !important;
 `;
 
 const contentWrap = css`
@@ -28,46 +30,81 @@ const contentWrap = css`
 `;
 
 const ArticleHeader = styled.header`
-    ${contentWrap};    
-    padding-top: 2em;
-    padding-bottom: 2em;
+    margin: auto 0 2em;
+    padding: 0 1em;
+    z-index: 201;
+    position: relative;
+    width: 100%;
+    ${ContentLimit};
+
     @media(min-width: 768px) {        
-        padding-top: 4em;
-        padding-bottom: 4em;
+        margin: auto 0 4em;
+        padding: 0;
     }
 
     & h1 {
         margin: 0 0 0.25em;
         font-size: 2em;
         letter-spacing: -0.5px;
-        color: ${mc.cyan.a400};
+        color: ${colorScheme.support};
+
         @media (min-width: 768px) {
-            font-size: 3em;
+            font-size: 2.5em;
+        }
+
+        @media (min-width: 1024px) {
+            font-size: 3.5em;
         }
     }
 `;
 
 const ArticleHero = styled.figure`
-    margin: 0;
-    padding: 0;
     width: 100%; 
-    height: 40vh;
-    margin: 0 auto 2em;
+    height: 50vh;
+    margin: 0 0 2em;
     overflow: hidden;
+    position: relative;
+    display: flex;
+    ${GridBase};
+
+    &::before {
+        content: '';
+        z-index: 101;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(to bottom,rgba(0,0,0,0.3) 0,rgba(0,0,0,0.9) 100%);
+
+        @media(min-width: 1024px) {
+            background: linear-gradient(to bottom,transparent 0,rgba(0,0,0,0.9) 100%);
+        }
+    }
 
     @media(min-width: 768px) {
         margin: 0 auto 4em;
     }
     
     @media(min-width: 1024px) {
-        width: calc(100% - 4em); 
-        height: 70vh;
+        width: 100%; 
+        margin: 0 0 4em;
+        height: 100vh;
     }
 `;
 
 const ArticleContent = styled.div`
-    ${contentWrap};
-    color: ${mc.grey[50]};
+    padding: 0 1em;
+    ${GridBase};
+    
+    @media(min-width: 768px) {
+        padding: 0;
+    }
+`;
+
+const ArticleEntryContent = styled.div`
+    color: ${colorScheme.text};
+    ${ContentLimit};
+    max-width: 70ch;
 
     & p {
         font-size: 1.25em;
@@ -77,9 +114,7 @@ const ArticleContent = styled.div`
         @media(min-width: 768px) {  
             font-size: 1.375em;
         }
-        & a {
-            color: ${mc.cyan.a400};
-        }
+        
         &:first-of-type {
             &:first-letter {
                 float: left;
@@ -88,7 +123,7 @@ const ArticleContent = styled.div`
                 font-style: normal;
                 font-size: 3.2em;
                 line-height: 0.7;
-                color: ${mc.cyan.a400};
+                color: ${colorScheme.support};
             }
         }
     }
@@ -101,23 +136,66 @@ const ArticleContent = styled.div`
         }
     }
     & .gatsby-highlight {
+        width: 100vw;
+        position: relative;
+        left: calc(-50vw + 50%);
+        margin: 2rem 0;
+
+        @media(min-width: 768px) {
+            margin: 4rem 0;
+            padding: 0 2rem;
+        }
+
+        @media(min-width: 1024px) {
+            left: -${((100 / 12) * 2)}vw;
+        }
+
+        @media(min-width: 1200px) {
+            width: calc(${100 - ((100 / 12) * 2)}vw - 2rem);
+            left: auto;
+            padding: 0;
+        }
+
+        @media(min-width: 1280px) {
+            width: calc(${100 - ((100 / 12) * 3)}vw - 2rem);
+        }
+
+        code, kbd, samp {
+            font-size: 1.25em;
+        }
+    }
+
+    .gatsby-resp-image-wrapper {
         position: relative;
         width: 100vw;
         left: calc(-50vw + 50%);
-        margin: 0 0 2.5em;
+        margin: 2rem 0;
+
+        @media(min-width: 768px) {
+           padding: 0 2rem;
+           margin: 4rem 0;
+        }
+
+        @media(min-width: 1024px) {
+            left: -${((100 / 12) * 2)}vw;
+        }
+
         @media(min-width: 1200px) {
-            width: 60vw;
-            left: calc(-30vw + 50%);
+            padding: 0;
+            width: calc(${100 - ((100 / 12) * 3)}vw - 2rem);
+            left: auto;
+            /* left: calc(-35vw + 50%); */
         }
     }
+
     & h1,h2,h3,h4,h5,h6 {
         font-weight: 700;
-        color: ${mc.cyan.a400};
+        color: ${colorScheme.support};
         line-height: 1.55;
     }
     h2 {
         font-size: 1.625em;
-        border-bottom: 1px solid ${mc.cyan.a400};
+        border-bottom: 1px solid ${colorScheme.support};
     }
     h3,h4,h5,h6  {
         font-size: 1.5em;
@@ -125,7 +203,7 @@ const ArticleContent = styled.div`
 `;
 
 const ExcerptMeta = styled.ul`
-    color: ${mc.blueGrey[100]};
+    color: ${colorScheme.text};
     font-style: italic;
     font-size: 1.125em;
     list-style: none;
@@ -159,7 +237,7 @@ const ExcerptMeta = styled.ul`
     }
     & a {
         text-decoration: none;
-        color: ${mc.blueGrey[100]};
+        color: ${colorScheme.text};
     }
 `;
 
@@ -198,9 +276,10 @@ const CatLink = styled(Link)`
     margin: 0 0 1em;
     font-size: 1.125em;
     font-style: italic;
-    display: block;
+    display: inline-block;
     text-decoration: none;
-    color: ${mc.blueGrey[100]};
+    color: ${colorScheme.text};
+
     &:hover {
         text-decoration: underline;
     }
@@ -208,7 +287,7 @@ const CatLink = styled(Link)`
 
 const TagPos = css`
     float: right;
-    background: ${mc.deepOrange[700]};
+    background: ${colorScheme.support};
 `;
 
 function preventWidow(string) {
@@ -221,43 +300,46 @@ const BlogPost = (props) => {
     return (
         <Article>
             <SEO type="post" post={props.data.markdownRemark} />
-            <ArticleHeader>
-                {!frontmatter.published &&
-                    <Tag style={TagPos} tagText="unpublished" />
-                }
-                <CatLink to={`/categories/${frontmatter.category}`}>{frontmatter.category}</CatLink>
-                <h1>
-                    {preventWidow(frontmatter.title)}
-                </h1>
-                <ExcerptMeta>
-                    <li>
-                        <span>{frontmatter.date}</span>
-                    </li>
-                    <li>
-                        <TagList>
-                            <span>Tagged:</span>
-                            <ul>
-                                {frontmatter.tags.map(tag => (
-                                    <li key={tag}>
-                                        <Link to={`/tags/${tag}`}>
-                                            {tag}
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </TagList>
-                    </li>
-                </ExcerptMeta>
-            </ArticleHeader>
             <ArticleHero>
                 <Img
                     outerWrapperClassName={imgStyle}
+                    position="absolute"
                     className={imgStyle}
                     resolutions={childImageSharp.resolutions}
                 />
+                <ArticleHeader>
+                    {!frontmatter.published &&
+                    <Tag style={TagPos} tagText="unpublished" />
+                    }
+                    <CatLink to={`/categories/${frontmatter.category}`}>{frontmatter.category}</CatLink>
+                    <h1>
+                        {preventWidow(frontmatter.title)}
+                    </h1>
+                    <ExcerptMeta>
+                        <li>
+                            <span>{frontmatter.date}</span>
+                        </li>
+                        <li>
+                            <TagList>
+                                <span>Tagged:</span>
+                                <ul>
+                                    {frontmatter.tags.map(tag => (
+                                        <li key={tag}>
+                                            <Link to={`/tags/${tag}`}>
+                                                {tag}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </TagList>
+                        </li>
+                    </ExcerptMeta>
+                </ArticleHeader>
             </ArticleHero>
             <ArticleContent>
-                <div dangerouslySetInnerHTML={{ __html: html }} />
+                <ArticleEntryContent>
+                    <div dangerouslySetInnerHTML={{ __html: html }} />
+                </ArticleEntryContent>
             </ArticleContent>
         </Article>
     );
