ee28f3109432442857088404f39a8cdb6cb83c83
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-08 02:53:25 +0000

    [add] global style setting exports

---
+++ src/helpers/styleSettings.js
@@ -0,0 +1,17 @@
+import mc from 'material-colors';
+
+export const colorScheme = {
+    dark: mc.blueGrey[900],
+    darkLight: mc.blueGrey[800],
+    support: mc.pink.a400,
+    supportDark: mc.pink[700],
+    text: '#ffffff',
+    logo: '#ffffff',
+    meta: mc.blueGrey[200],
+};
+
+export const fontScheme = {
+    text: '\'ff-tisa-web-pro\', georgia, serif',
+    headings: '\'proxima-nova\', sans-serif',
+};
+
