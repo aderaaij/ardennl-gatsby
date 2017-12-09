b9fdb0e4ac28c2a4e44fe5374b8166bc77a99422
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-09 03:50:04 +0000

    [add] ExcerptMeta component, fix hover issue

--- src/components/PostExcerpt/PostExcerpt.js
+++ src/components/PostExcerpt/PostExcerpt.js
@@ -4,20 +4,18 @@ import Transition from 'react-transition-group/Transition';
 import Link, { navigateTo } from 'gatsby-link';
 import styled, { css } from 'react-emotion';
 import Tag from '../TagLabel/TagLabel';
+import TagList from '../TagList/TagList';
+import ExcerptMeta from '../ExcerptMeta/ExcerptMeta';
 import { colorScheme } from '../../helpers/styleSettings';
 
 const BlogArticle = styled.article`
-    z-index: 201;
     position: relative;
-    display: block;    
-    margin: 0 auto;
+    z-index: 201;
     padding: 2em 1em;
     cursor: pointer;
-    transition: background 0.3s ease-out;
     border-bottom: 1px solid ${colorScheme.meta};
 
     @media (min-width: 768px) {
-        display: flex;
         padding: 4em 0;
         width: 100%;
     }
@@ -48,84 +46,6 @@ const BlogContent = styled.div`
 
         @media (min-width: 768px) {
             font-size: 1.25em;
-        }
-    }
-`;
-
-const ExcerptMeta = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    line-height: 1.5;
-    font-size: 1rem;
-    font-style: italic;   
-    text-transform: lowercase; 
-    color: ${colorScheme.meta};
-
-    @media (min-width: 768px) {
-        display: flex;
-        line-height: 1;
-    }
-
-    & > li {
-        font-size: 1rem; 
-
-        @media (min-width: 768px) {
-            &::after {
-                content: '|';
-                margin: 0 0.5em;
-            }
-        }
-
-        &:last-child {
-            display: flex;
-
-            &::after {
-                content: '';
-                margin: 0;
-            }
-        }
-    }
-
-    & a {
-        text-decoration: none;
-        color: ${colorScheme.text};
-    }
-`;
-
-const TagList = styled.div`
-    display: flex;
-
-    span {
-        margin-right: 1ch;
-    }
-
-    ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
-    }   
-
-    a {
-        text-decoration: none;
-
-        &:hover {
-            text-decoration: underline;
-        }
-    }
-
-    li {
-        &::after {
-            content: ',';
-            margin-right: 1ch;
-        }
-
-        &:last-child {
-            &::after {
-                content: '';
-                margin: 0;
-            }
         }
     }
 `;
@@ -171,7 +91,6 @@ const BGImage = css`
     }
 `;
 
-
 const transitionStyles = {
     entering: { opacity: 0 },
     entered: { opacity: 1 },
@@ -184,6 +103,14 @@ function goToPage(e, slug) {
         navigateTo(slug);
     }
 }
+
+const ExcerptMetaStyle = css`
+    color: ${colorScheme.meta};
+
+    a {
+        color: ${colorScheme.meta};
+    }
+`;
 
 const Fade = ({ in: isHovering, image }) => (
     <Transition in={isHovering} timeout={10}>
@@ -210,30 +137,25 @@ export default class PostExcerpt extends Component {
     }
 
     handleMouseEnter() {
-        this.setState(() => ({
+        this.setState({
             isHovering: true,
-        }));
+        });
     }
-
     handleMouseLeave() {
-        this.setState(() => ({
+        this.setState({
             isHovering: false,
-        }));
-    }
-
-    toggleHoverState() {
-        return {
-            isHovering: !this.state.isHovering,
-        };
+        });
     }
 
     render() {
         const { postInfo } = this.props;
         const { isHovering } = this.state;
-        console.log(this.state);
         return (
             <div>
-                <Fade in={!!isHovering} image={postInfo.cover.childImageSharp.resolutions.tracedSVG} />
+                <Fade
+                    in={!!isHovering}
+                    image={postInfo.cover.childImageSharp.resolutions.tracedSVG}
+                />
                 <BlogArticle
                     onMouseEnter={() => this.handleMouseEnter()}
                     onMouseLeave={() => this.handleMouseLeave()}
@@ -242,32 +164,23 @@ export default class PostExcerpt extends Component {
                     <BlogContent>
                         <div>
                             {!postInfo.published &&
-                            <Tag style={TagPos} tagText="unpublished" />
+                            <Tag
+                                style={TagPos}
+                                tagText="unpublished"
+                            />
                             }
                             <CatLink to={`/categories/${postInfo.category}`}>{postInfo.category}</CatLink>
                             <h2>
                                 <Link to={postInfo.path}>{postInfo.title}</Link>
                             </h2>
                             <p>{postInfo.excerpt}</p>
-                            <ExcerptMeta>
-                                <li>
-                                    <span>{postInfo.date}</span>
-                                </li>
-                                <li>
-                                    <TagList>
-                                        <span>Tagged:</span>
-                                        <ul>
-                                            {postInfo.tags.map(tag => (
-                                                <li key={tag}>
-                                                    <Link to={`/tags/${tag}`}>
-                                                        {tag}
-                                                    </Link>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </TagList>
-                                </li>
-                            </ExcerptMeta>
+                            {(postInfo.tags || postInfo.date) &&
+                            <ExcerptMeta
+                                className={ExcerptMetaStyle}
+                                tags={postInfo.tags}
+                                date={postInfo.date}
+                            />
+                            }
                         </div>
                     </BlogContent>
                 </BlogArticle>
