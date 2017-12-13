2485567c90b339c0e4305e7ca0f25998fdab70b8
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-13 02:25:54 +0000

    [add] graphql default archive query in fragment

--- src/graphql/archive.js
+++ src/graphql/archive.js
@@ -1,36 +1,31 @@
 export const archiveQuery = graphql`
-    fragment commonArchiveQuery on ArchivePage {
-        totalCount
-        edges {
-            node {
-                fields {
-                    slug
-                }
-                excerpt
-                timeToRead
-                frontmatter {
-                    title
-                    tags
-                    category
-                    published
-                    date(formatString: "DD MMMM, YYYY")
-                    cover {
-                        id
-                        relativePath
-                        childImageSharp {
-                            resolutions(
-                                width: 1200,
-                                traceSVG: {
-                                    color: "#37474F",
-                                    blackOnWhite: false,
-                                }
-                            ) {
-                                tracedSVG
-                            }
+    fragment defaultArchiveQuery on MarkdownRemark {
+        fields {
+            slug
+        }
+        excerpt
+        timeToRead
+        frontmatter {
+            title
+            tags
+            category
+            published
+            date(formatString: "DD MMMM, YYYY")
+            cover {
+                id
+                relativePath
+                childImageSharp {
+                    resolutions(
+                        width: 1200,
+                        traceSVG: {
+                            color: "#37474F",
+                            blackOnWhite: false,
                         }
+                    ) {
+                        tracedSVG
                     }
                 }
             }
-        }
+        }        
     }
 `;
