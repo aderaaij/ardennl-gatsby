0a5c6e4981985442997f83052b9c596d7c70e4f8
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-13 02:27:42 +0000

    [add] default postquery as graphql fragment

--- src/graphql/post.js
+++ src/graphql/post.js
@@ -1,36 +1,36 @@
-export const postQuery = graphql`
-    fragment commonPostQuery on postPage {
-        html
-        timeToRead
-        excerpt
-        fields {
-            slug
-        }
-        frontmatter {
-            title
-            published
-            date(formatString: "DD MMMM, YYYY")
-            category
-            tags
-            cover {
-                id
-                childImageSharp {
-                    resolutions(
-                        width: 1200,
-                        traceSVG: {
-                            color: "#37474F",
-                            blackOnWhite: false,
-                        }
-                    ) {
-                        tracedSVG
-                        aspectRatio
-                        src
-                        srcSet
-                        srcWebp
-                        srcSetWebp
+export const defaultPostQuery = graphql`
+fragment postQuery on MarkdownRemark {
+    html
+    timeToRead
+    excerpt
+    fields {
+        slug
+    }
+    frontmatter {
+        title
+        published
+        date(formatString: "DD MMMM, YYYY")
+        category
+        tags
+        cover {
+            id
+            childImageSharp {
+                resolutions(
+                    width: 1200,
+                    traceSVG: {
+                        color: "#37474F",
+                        blackOnWhite: false,
                     }
+                ) {
+                    tracedSVG
+                    aspectRatio
+                    src
+                    srcSet
+                    srcWebp
+                    srcSetWebp
                 }
             }
         }
     }
+}
 `;
