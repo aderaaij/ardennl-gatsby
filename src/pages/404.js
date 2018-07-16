78637cb55c69896054254ee26b70e9dcf1727612
Arden de Raaij <a.de.raaij@gmail.com>
2018-07-16 20:42:55 +0100

    [fix] 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,76 +5,88 @@ import { colorScheme } from '../helpers/styleSettings';
 import { GridBase, ContentLimit } from '../helpers/grid';
 
 const NotFoundWrap = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    color: ${colorScheme.text};
-    ${GridBase};
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: ${colorScheme.text};
+  ${GridBase};
 
-    h1 {
-        font-size: 2em;
-        margin: 0;
-        color: ${colorScheme.support};
-        @media (min-width: 768px) {
-            font-size: 3em;
-        }
+  h1 {
+    font-size: 2em;
+    margin: 0;
+    color: ${colorScheme.support};
+    @media (min-width: 768px) {
+      font-size: 3em;
     }
+  }
 
-    p {
-        font-size: 1.25em;
-        line-height: 1.55;
-    }
+  p {
+    font-size: 1.25em;
+    line-height: 1.55;
+  }
 `;
 
 const NotFoundimage = styled.img`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    object-fit: cover;
-    z-index: -1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+  z-index: -1;
 `;
 
 const NotFoundContent = styled.div`
-    ${ContentLimit};
+  ${ContentLimit};
 `;
 
-const NotFoundPage = ({ data }) => (
+const NotFoundPage = ({ data }) => {
+  const { edges } = data.allFile;
+  const bg = edges.find(edge => edge.node.name.includes('arden'));
+  return (
     <NotFoundWrap>
-        <NotFoundimage src={data.imageSharp.sizes.tracedSVG} />
-        <NotFoundContent>
-            <h1>404 - NOT FOUND</h1>
-            <p>This page does not exist. Maybe it never has.</p>
-        </NotFoundContent>
+      <NotFoundimage src={bg.node.childImageSharp.fluid.tracedSVG} />
+      <NotFoundContent>
+        <h1>404 - NOT FOUND</h1>
+        <p>This page does not exist. Maybe it never has.</p>
+      </NotFoundContent>
     </NotFoundWrap>
-);
+  );
+};
 
 NotFoundPage.propTypes = {
-    data: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired
 };
 
 export default NotFoundPage;
 
 export const query = graphql`
-    query errorQuery {
-        imageSharp(id: { regex: "/404/" }) {
-            sizes(
-                maxWidth: 1600
-                traceSVG: {
-                    optTolerance: 0.1
-                    turdSize: 0.1
-                    alphaMax: 0.01
-                    color: "#37474F"
-                    threshold: 170
-                    blackOnWhite: false
-                }
-            ) {
-                tracedSVG
+  query errorQuery {
+    allFile(filter: { name: { eq: "arden" } }) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            id
+            fluid(traceSVG: { blackOnWhite: false, color: "#37474F" }) {
+              base64
+              tracedSVG
+              aspectRatio
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+              sizes
+              originalImg
+              originalName
             }
+          }
         }
+      }
     }
+  }
 `;
