fa08d71a1bbd2fb7483705d1f79960517b3ef44a
Arden de Raaij <a.de.raaij@gmail.com>
2019-02-10 17:22:41 +0000

    [update] lint rules

--- src/helpers/global.ts
+++ src/helpers/global.ts
@@ -1,5 +1,5 @@
 import { css } from '@emotion/core';
-import { fontScheme, colorScheme } from './styleSettings';
+import { colorScheme, fontScheme } from './styleSettings';
 
 export const globalCss = css`
   * {
