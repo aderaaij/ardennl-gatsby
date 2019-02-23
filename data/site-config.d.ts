b545986a01ef427ce6f82212fd27697c79cfe598
Arden de Raaij <a.de.raaij@gmail.com>
2019-02-23 17:05:28 +0000

    [add] config file type definition

---
+++ data/site-config.d.ts
@@ -0,0 +1,23 @@
+declare const siteConfig: {
+  siteName: string;
+  siteLogo: string;
+  siteUrl: string;
+  siteTitle: string;
+  siteDescription: string;
+  siteLanguage: string;
+  siteLinks: {
+    Github: {
+      handle: string;
+    };
+    Twitter: {
+      handle: string;
+    };
+    instagram: {
+      handle: string;
+    };
+    email: {
+      email: string;
+    };
+  };
+};
+export = siteConfig;
