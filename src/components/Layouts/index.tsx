f594e584ec02a8e749c09fe9d87ddcc30a91be52
Arden de Raaij <a.de.raaij@gmail.com>
2021-07-05 18:24:28 +0100

    remove analytics

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -5,7 +5,6 @@ import 'normalize.css';
 
 import config from '../../../config/site-config';
 import SiteHeader from 'src/components/SiteHeader';
-import Piwik from 'src/components/Piwik';
 import Typekit from 'src/components/Typekit';
 import { globalCss } from 'src/helpers/global';
 
@@ -13,7 +12,6 @@ const TemplateWrapper = ({ children }: any) => (
   <>
     <Global styles={globalCss} />
     <Helmet title={config.siteDescription} htmlAttributes={{ lang: 'EN' }} />
-    <Piwik />
     <Typekit />
     <SiteHeader />
     {children}
